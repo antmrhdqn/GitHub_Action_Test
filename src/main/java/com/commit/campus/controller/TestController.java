dd30d2158858a921cc0509ef610a00f65365da9c
antmrhdqn <antmrhdqn@gmail.com>
2024-08-15 16:09:06 +0900

    테스트 메소드 추가

--- src/main/java/com/commit/campus/controller/TestController.java
+++ src/main/java/com/commit/campus/controller/TestController.java
@@ -1,13 +1,15 @@
 package com.commit.campus.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class TestController {
 
     @GetMapping("test")
-    public String Test() {
-        return "성공";
+    public void Test() {
+        log.info("성공");
     }
 }
