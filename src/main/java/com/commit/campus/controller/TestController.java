71a59259f5b7fb08748f4ecc7fab056deb16a31e
antmrhdqn <antmrhdqn@gmail.com>
2024-08-15 20:55:49 +0900

    수정

--- src/main/java/com/commit/campus/controller/TestController.java
+++ src/main/java/com/commit/campus/controller/TestController.java
@@ -3,12 +3,12 @@ package com.commit.campus.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
 public class TestController {
-
-    @GetMapping("test")
+    @PostMapping("test")
     public void Test() {
         log.info("성공");
     }
